0eb20993dd03dc5deec04eb79b37834333383c9d
Jo <jo3cyc@gmail.com>
2016-05-28 14:06:06 +0800

    Added animation things in game.html

---
+++ cursor.js
@@ -0,0 +1,23 @@
+﻿function image(id, name) {
+    this.id = id;
+    this.name = name;
+}
+
+var allImages = [];
+var folder = "images/";
+
+var id = 0;
+$.ajax({
+    url: folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if (val.match(/\.(jpe?g|png|gif)$/)) {
+                //$("body").append("<img src='" + folder + val + "'>");
+                allImages.push(new image(id++, val));
+            }
+        });
+    }
+});
+
+for(var i in allImages)
+    console.log(allImages);
---
+++ triple-clone-game.js
@@ -0,0 +1,89 @@
+﻿/** 
+ * Base class for ALL drawable objects in the game
+ */
+function Drawable() {
+    this.init = function (x, y) {
+        this.x = x;
+        this.y = y;
+        this.id = 0;
+    }
+
+    this.draw = function () {
+    };
+}
+
+/**
+ * Background object
+ * Do static for now, later feature may includes minor animation or day change?
+ */
+function Backgound() {
+
+    this.draw = function () {
+
+    }
+}
+
+/**
+ * The animation loop. Calls the requestAnimationFrame shim to
+ * optimize the game loop and draws all game objects. This
+ * function must be a gobal function and cannot be within an
+ * object.
+ * http://blog.sklambert.com/html5-canvas-game-panning-a-background/
+ */
+function animate() {
+    requestAnimFrame(animate);
+    game.background.draw();
+}
+
+/**
+ * requestAnim shim layer by Paul Irish
+ * Finds the first API that works to optimize the animation loop,
+ * otherwise defaults to setTimeout().
+ * http://blog.sklambert.com/html5-canvas-game-panning-a-background/
+ */
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function (/* function */ callback, /* DOMElement */ element) {
+			    window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+/**
+ * Game object
+ */
+function Game() {
+/*
+ * Gets canvas info and context
+ * Returns true if canvas is supported, and false if not and nothing is loaded
+ */
+    this.init = function () {
+        this.bgCanvas = document.getElementById('bgCanvas');
+
+        if (this.bgCanvas.getContext) {
+            this.bgContext = this.bgCanvas.getContext('2d');
+
+            Backgound.prototype.context = this.bgContext;
+            Backgound.prototype.canvasWidth = this.bgCanvas.width;
+            Backgound.prototype.canvasHeight = this.bgCanvas.height;
+
+            // init background object
+            this.background = new Backgound();
+            this.background.init(0, 0);
+
+            // TODO: init the grid and items and etc
+
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.start = function () {
+        animate();
+    }
+
+}
